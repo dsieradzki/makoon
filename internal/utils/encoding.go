f140efbc27806ce71a982f5a1c6b465ad80e0a12
Damian Sieradzki <damian.sieradzki@hotmail.com>
2022-07-17 15:13:36 +0200

    fix: encoding password with special characters

--- internal/utils/encoding.go
+++ internal/utils/encoding.go
@@ -9,5 +9,6 @@ func PathEscape(v string) string {
 	r := url.PathEscape(v)
 	r = strings.ReplaceAll(r, "+", "%2B")
 	r = strings.ReplaceAll(r, "=", "%3D")
+	r = strings.ReplaceAll(r, "&", "%26")
 	return r
 }
