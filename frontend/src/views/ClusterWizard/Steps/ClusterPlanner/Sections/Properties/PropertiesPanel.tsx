50775401dcf796dd06e7a14a8c3e517a02260ccb
Damian Sieradzki <damian.sieradzki@hotmail.com>
2022-09-17 01:38:30 +0200

    fix: blank screen on load properties panels, update ubuntu cloud image

--- frontend/src/views/ClusterWizard/Steps/ClusterPlanner/Sections/Properties/PropertiesPanel.tsx
+++ frontend/src/views/ClusterWizard/Steps/ClusterPlanner/Sections/Properties/PropertiesPanel.tsx
@@ -1,10 +1,23 @@
 import React from "react";
 
+
+const panels: { [key: string]: React.LazyExoticComponent<React.FC> } = {
+    "ArgoCdProperties": React.lazy(() => import("./ArgoCdProperties")),
+    "CustomHelmAppProperties": React.lazy(() => import("./CustomHelmAppProperties")),
+    "CustomK8sResourceProperties": React.lazy(() => import("./CustomK8sResourceProperties")),
+    "IngressProperties": React.lazy(() => import("./IngressProperties")),
+    "MetalLbProperties": React.lazy(() => import("./MetalLbProperties")),
+    "MetricsServerProperties": React.lazy(() => import("./MetricsServerProperties")),
+    "NodeProperties": React.lazy(() => import("./NodeProperties")),
+    "OpenEbsProperties": React.lazy(() => import("./OpenEbsProperties")),
+    "PortainerProperties": React.lazy(() => import("./PortainerProperties"))
+};
 type Props = {
     componentName: string
 }
 const PropertiesPanel = (props: Props) => {
-    const PropertiesComponent = React.lazy(() => import("./" + props.componentName));
+
+    const PropertiesComponent = panels[props.componentName]
     return <React.Suspense>
         <PropertiesComponent/>
     </React.Suspense>
