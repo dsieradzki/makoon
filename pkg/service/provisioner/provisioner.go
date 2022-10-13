565bd783d51d1bad81864de29f09f695f262dc99
Damian Sieradzki <damian.sieradzki@hotmail.com>
2022-10-14 00:43:37 +0200

    fix: add more logs, event for saving kubernetes config, update dependencies

--- pkg/service/provisioner/provisioner.go
+++ pkg/service/provisioner/provisioner.go
@@ -7,6 +7,7 @@ import (
 	"github.com/dsieradzki/k4prox/internal/proxmox"
 	"github.com/dsieradzki/k4prox/internal/ssh"
 	"github.com/dsieradzki/k4prox/pkg/service/project"
+	log "github.com/sirupsen/logrus"
 	"sort"
 )
 
@@ -16,16 +17,18 @@ func NewService(
 	sshClient *ssh.Client,
 	eventCollector *event.Collector) *Service {
 	return &Service{
-		project:       projectService,
-		proxmoxClient: proxmoxClient,
-		k4p:           k4p.NewK4PService(proxmoxClient, sshClient, eventCollector),
+		eventCollector: eventCollector,
+		project:        projectService,
+		proxmoxClient:  proxmoxClient,
+		k4p:            k4p.NewK4PService(proxmoxClient, sshClient, eventCollector),
 	}
 }
 
 type Service struct {
-	k4p           *k4p.Service
-	proxmoxClient *proxmox.Client
-	project       *project.Service
+	eventCollector *event.Collector
+	k4p            *k4p.Service
+	proxmoxClient  *proxmox.Client
+	project        *project.Service
 }
 
 func (p *Service) SetupEnvironmentOnProxmox() error {
@@ -35,42 +38,50 @@ func (p *Service) SetupEnvironmentOnProxmox() error {
 func (p *Service) CreateCluster(provisionRequest k4p.ProvisionRequest) error {
 	projectData, err := p.project.LoadProject()
 	if err != nil {
+		log.WithError(err).Error("Cannot load project")
 		return err
 	}
 	if projectData.SshKey.Empty() {
 		rsaKeyPair, err := ssh.GenerateRsaKeyPair()
 		if err != nil {
+			log.WithError(err).Error("Cannot generate RSA key pair")
 			return err
 		}
 		projectData.SshKey = rsaKeyPair
 		err = p.project.SaveProject(projectData)
 		if err != nil {
+			log.WithError(err).Error("Cannot save project file")
 			return err
 		}
 	}
 	if provisionRequest.Stages.CreateVirtualMachines {
 		err = p.k4p.CreateVirtualMachines(projectData.Cluster, projectData.SshKey)
 		if err != nil {
+			log.WithError(err).Error("Cannot create virtual machines")
 			return err
 		}
 
 		err = p.k4p.StartVirtualMachines(projectData.Cluster, projectData.SshKey)
 		if err != nil {
+			log.WithError(err).Error("Cannot start virtual machines")
 			return err
 		}
 
 		err = p.k4p.UpdateVmsOs(projectData.Cluster, projectData.SshKey)
 		if err != nil {
+			log.WithError(err).Error("Cannot update virtual machine OS")
 			return err
 		}
 
 		err = p.k4p.ShutdownVirtualMachines(projectData.Cluster)
 		if err != nil {
+			log.WithError(err).Error("Cannot shutdown virtual machines")
 			return err
 		}
 
 		err = p.k4p.StartVirtualMachines(projectData.Cluster, projectData.SshKey)
 		if err != nil {
+			log.WithError(err).Error("Cannot start virtual machines")
 			return err
 		}
 	}
@@ -78,6 +89,7 @@ func (p *Service) CreateCluster(provisionRequest k4p.ProvisionRequest) error {
 	if provisionRequest.Stages.SetupVirtualMachines {
 		err = p.k4p.SetupVmsOs(projectData.Cluster, projectData.SshKey)
 		if err != nil {
+			log.WithError(err).Error("Cannot setup virtual machine OS")
 			return err
 		}
 	}
@@ -85,12 +97,14 @@ func (p *Service) CreateCluster(provisionRequest k4p.ProvisionRequest) error {
 	if provisionRequest.Stages.InstallKubernetes {
 		err = p.k4p.InstallKubernetesOnNodes(projectData.Cluster, projectData.SshKey)
 		if err != nil {
+			log.WithError(err).Error("Cannot install Kubernetes")
 			return err
 		}
 	}
 	if provisionRequest.Stages.JoinNodesToCluster {
 		err = p.k4p.JoinNodesToCluster(projectData.Cluster, projectData.SshKey)
 		if err != nil {
+			log.WithError(err).Error("Cannot join node to cluster")
 			return err
 		}
 	}
@@ -98,41 +112,52 @@ func (p *Service) CreateCluster(provisionRequest k4p.ProvisionRequest) error {
 	if provisionRequest.Stages.InstallKubernetes {
 		kubeConfigContent, err := p.k4p.GetKubeConfigFromCluster(projectData.Cluster, projectData.SshKey)
 		if err != nil {
+			log.WithError(err).Error("Cannot get Kubernetes config from cluster")
 			return err
 		}
+		eventSession := p.eventCollector.Start("Add Kubernetes config to project file")
 		projectDataToUpdate, err := p.project.LoadProject()
 		if err != nil {
+			log.WithError(err).Error("Cannot load project file")
+			eventSession.ReportError(err)
 			return err
 		}
 		projectDataToUpdate.KubeConfig = kubeConfigContent
 		projectData = projectDataToUpdate
 		err = p.project.SaveProject(projectDataToUpdate)
 		if err != nil {
+			log.WithError(err).Error("Cannot save project file")
+			eventSession.ReportError(err)
 			return err
 		}
+		eventSession.Done()
 	}
 
 	if provisionRequest.Stages.InstallAddons {
 		err = p.k4p.InstallAddons(projectData.Cluster, projectData.SshKey)
 		if err != nil {
+			log.WithError(err).Error("Cannot install addons")
 			return err
 		}
 	}
 	if provisionRequest.Stages.InstallHelmApps {
 		err = p.k4p.InstallHelmApps(projectData.Cluster, projectData.SshKey)
 		if err != nil {
+			log.WithError(err).Error("Cannot install Helm applications")
 			return err
 		}
 	}
 	if provisionRequest.Stages.InstallCustomHelmApps {
 		err = p.k4p.InstallCustomHelmApps(projectData.Cluster, projectData.SshKey)
 		if err != nil {
+			log.WithError(err).Error("Cannot install custom Helm applications")
 			return err
 		}
 	}
 	if provisionRequest.Stages.InstallCustomK8sResources {
 		err = p.k4p.InstallAdditionalK8sResources(projectData.Cluster, projectData.SshKey)
 		if err != nil {
+			log.WithError(err).Error("Cannot install additional Kubernetes resources")
 			return err
 		}
 	}
@@ -143,6 +168,7 @@ func (p *Service) CreateCluster(provisionRequest k4p.ProvisionRequest) error {
 func (p *Service) GetNetworkBridges() ([]string, error) {
 	networks, err := p.proxmoxClient.GetNetworkBridges(p.proxmoxClient.GetProxmoxNodeName())
 	if err != nil {
+		log.WithError(err).Error("Cannot get information about network devices")
 		return []string{}, err
 	}
 	netNames := collect.Map(networks, func(n proxmox.Network) string {
@@ -155,6 +181,7 @@ func (p *Service) GetNetworkBridges() ([]string, error) {
 func (p *Service) GetStorage() ([]string, error) {
 	storage, err := p.proxmoxClient.GetStorage()
 	if err != nil {
+		log.WithError(err).Error("Cannot get information about storage")
 		return []string{}, err
 	}
 	storageNames := collect.Map(storage, func(i proxmox.Storage) string {
